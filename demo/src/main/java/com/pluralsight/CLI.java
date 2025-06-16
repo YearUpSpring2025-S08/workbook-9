7c8a30c65e5130a9b7a34abc4808a9fee6ba853a
mattchristenson <matt@rcc.team>
2025-06-16 10:26:33 -0500

    before break

---
+++ demo/src/main/java/com/pluralsight/CLI.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CLI implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args){
+        System.out.println("Hello World from CLI");
+    }
+
+
+
+}
---
+++ demo/src/main/java/com/pluralsight/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+
+import com.pluralsight.dao.RegistrationDAO;
+import com.pluralsight.dao.SimpleRegistrationDAO;
+import com.pluralsight.model.Student;
+import com.pluralsight.service.RegistrationService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) throws Exception {
+		ApplicationContext context;
+		context = SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
