b161c2de4ccd393264787b927e13b6e10f2ef4d7
mattchristenson <matt@rcc.team>
2025-06-16 12:45:54 -0500

    refactored into CommandLineRunner

--- NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplication.java
+++ NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplication.java
@@ -13,44 +13,13 @@ import java.util.List;
 public class NorthwindTradersSpringBootApplication {
 
 	private static Console console = new Console();
+	private static ApplicationContext appContext;
 
 	public static void main(String[] args) {
-		ApplicationContext appContext =	SpringApplication.run(NorthwindTradersSpringBootApplication.class, args);
-
-
-
-		while (true) {
-			System.out.println();
-			System.out.println();
-
-			String[] menuOptions = {
-					"List Products",
-					"Add Product",
-					"exit"
-			};
-
-			int userChoice = console.promptForOption(menuOptions);
-			switch (userChoice) {
-				case 1:
-				//	listProducts();
-					ProductDAO productDAO = appContext.getBean(ProductDAO.class);
-					List<Product> products = productDAO.getAll();
-					for(Product p : products){
-						System.out.println(p);
-					}
-					break;
-				case 2:
-				//	addProduct();
-					break;
-				case 3:
-					System.exit(0);
-			}
-
-
-			console.promptForString("Please press <ENTER> to continue...", true);
-		}
-
+		appContext = SpringApplication.run(NorthwindTradersSpringBootApplication.class, args);
 
 	}
+
+
 
 }
