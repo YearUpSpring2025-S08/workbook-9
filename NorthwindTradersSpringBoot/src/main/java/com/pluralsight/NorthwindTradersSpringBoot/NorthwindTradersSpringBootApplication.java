fc1a9b8a3e1a7d3078753d467a86696d3145ab7c
mattchristenson <matt@rcc.team>
2025-06-16 12:57:54 -0500

    before transition to configuration

--- NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplication.java
+++ NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplication.java
@@ -12,14 +12,8 @@ import java.util.List;
 @SpringBootApplication
 public class NorthwindTradersSpringBootApplication {
 
-	private static Console console = new Console();
-	private static ApplicationContext appContext;
-
 	public static void main(String[] args) {
-		appContext = SpringApplication.run(NorthwindTradersSpringBootApplication.class, args);
-
+		SpringApplication.run(NorthwindTradersSpringBootApplication.class, args);
 	}
-
-
 
 }
