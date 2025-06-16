ecf448a30eed91eab96086d4bbfa5f7080569e6d
mattchristenson <matt@rcc.team>
2025-06-16 12:18:19 -0500

    after lunch

--- NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplication.java
+++ NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplication.java
@@ -1,13 +1,56 @@
 package com.pluralsight.NorthwindTradersSpringBoot;
 
+import com.pluralsight.NorthwindTradersSpringBoot.data.ProductDAO;
+import com.pluralsight.NorthwindTradersSpringBoot.data.SimpleProductDAO;
+import com.pluralsight.NorthwindTradersSpringBoot.models.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication
 public class NorthwindTradersSpringBootApplication {
 
+	private static Console console = new Console();
+
 	public static void main(String[] args) {
-		SpringApplication.run(NorthwindTradersSpringBootApplication.class, args);
+		ApplicationContext appContext =	SpringApplication.run(NorthwindTradersSpringBootApplication.class, args);
+
+
+
+		while (true) {
+			System.out.println();
+			System.out.println();
+
+			String[] menuOptions = {
+					"List Products",
+					"Add Product",
+					"exit"
+			};
+
+			int userChoice = console.promptForOption(menuOptions);
+			switch (userChoice) {
+				case 1:
+				//	listProducts();
+					ProductDAO productDAO = appContext.getBean(ProductDAO.class);
+					List<Product> products = productDAO.getAll();
+					for(Product p : products){
+						System.out.println(p);
+					}
+					break;
+				case 2:
+				//	addProduct();
+					break;
+				case 3:
+					System.exit(0);
+			}
+
+
+			console.promptForString("Please press <ENTER> to continue...", true);
+		}
+
+
 	}
 
 }
--- NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/models/Product.java
+++ NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/models/Product.java
@@ -51,4 +51,13 @@ public class Product implements Serializable {
         this.price = price;
     }
 
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
