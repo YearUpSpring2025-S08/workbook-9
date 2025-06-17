3868a5880193087f5e77e36e335104b4c65d4218
mattchristenson <matt@rcc.team>
2025-06-17 14:06:56 -0500

    jdbc provider created for products

--- NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/dao/JdbcProductDAO.java
+++ NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/dao/JdbcProductDAO.java
@@ -1,12 +1,32 @@
 package com.pluralsight.NorthwindTradersAPI.dao;
 
 import com.pluralsight.NorthwindTradersAPI.models.Product;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class JdbcProductDAO implements ProductDAO {
+
+
+    private DatabaseConfig databaseConfig;
+
+    @Autowired
+    public JdbcProductDAO(DatabaseConfig databaseConfig) {
+        this.databaseConfig = databaseConfig;
+    }
+
     @Override
     public List<Product> getAllProducts() {
+
+        ArrayList<Product> products = new ArrayList<>();
 
         String query = """
                  select
@@ -18,12 +38,35 @@ public class JdbcProductDAO implements ProductDAO {
                                 products
                 """;
 
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(databaseConfig.getUrl());
+        basicDataSource.setUsername(databaseConfig.getUsername());
+        basicDataSource.setPassword(databaseConfig.getPassword());
 
-        return List.of();
+        try(
+                Connection connection = basicDataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+                ResultSet resultSet = preparedStatement.executeQuery();)
+        {
+            while(resultSet.next()){
+                int productId = resultSet.getInt(1);
+                String productName = resultSet.getString(2);
+                int categoryId = resultSet.getInt(3);
+                double price = resultSet.getDouble(4);
+                Product p = new Product(productId, productName, categoryId, price);
+                products.add(p);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return products;
+
     }
 
     @Override
     public Product getProductById(int productId) {
+
         String query = """
                 select
                                productId,
@@ -33,7 +76,36 @@ public class JdbcProductDAO implements ProductDAO {
                                from
                                products
                                WHERE ProductID = ?""";
+
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(databaseConfig.getUrl());
+        basicDataSource.setUsername(databaseConfig.getUsername());
+        basicDataSource.setPassword(databaseConfig.getPassword());
+
+        try(
+                Connection connection = basicDataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+        )
+        {
+            preparedStatement.setInt(1, productId);
+            try(ResultSet resultSet = preparedStatement.executeQuery()){
+                while(resultSet.next()){
+                    String productName = resultSet.getString(2);
+                    int categoryId = resultSet.getInt(3);
+                    double price = resultSet.getDouble(4);
+                    Product p = new Product(productId, productName, categoryId, price);
+                    return p;
+                }
+            }
+
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return null;
+
     }
 
     @Override
